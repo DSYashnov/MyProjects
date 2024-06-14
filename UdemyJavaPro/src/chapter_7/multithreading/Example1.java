0943ec5258a5127154326a5434db75073533ec76
Stanislav Yashnov <s.yashnov@gmail.com>
2024-06-14 20:26:14 +0300

    Udemy3

---
+++ UdemyJavaPro/src/chapter_7/multithreading/Example1.java
@@ -0,0 +1,11 @@
+package chapter_7.multithreading;
+
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("bye");
+    }
+}
