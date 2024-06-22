393971e0774d394a73dc9da12de34e6788a37700
Stanislav Yashnov <s.yashnov@gmail.com>
2024-06-23 00:10:33 +0300

    Udemy3

--- UdemyJavaPro/src/chapter_7/threadMethods/daemon/DaemonExample.java
+++ UdemyJavaPro/src/chapter_7/threadMethods/daemon/DaemonExample.java
@@ -2,6 +2,42 @@ package chapter_7.threadMethods.daemon;
 
 public class DaemonExample {
     public static void main(String[] args) {
-
+        System.out.println("Main thread starts");
+        UserThread userThread = new UserThread();
+        userThread.setName("UserThread");
+        DaemonThread daemonThread = new DaemonThread();
+        daemonThread.setName("DaemonThread");
+        daemonThread.setDaemon(true);
+        userThread.start();
+        daemonThread.start();
+        System.out.println("Main thread ends");
+    }
+}
+class UserThread extends Thread {
+    public void run() {
+        System.out.println(Thread.currentThread().getName() +
+                " is daemon: " + isDaemon());
+        for(char i = 'A'; i <= 'J'; i++) {
+            try {
+                sleep(300);
+                System.out.println(i);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+class DaemonThread extends Thread {
+    public void run() {
+        System.out.println(Thread.currentThread().getName() +
+                " is daemon: " + isDaemon());
+        for(int i = 1; i <= 1000; i++) {
+            try {
+                sleep(100);
+                System.out.println(i);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
