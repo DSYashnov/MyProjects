7f98cfc7e86aeb8fcfaec0cc7bb55a39cdd43cd3
syash <s.yashnov@gmail.com>
2024-07-01 17:51:49 +0300

    les30

---
+++ UdemyJavaPro/src/work/chapter_8/fileReader/FileReaderExample.java
@@ -0,0 +1,20 @@
+package work.chapter_8.fileReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileReaderExample {
+    public static void main(String[] args) throws IOException {
+        try (FileReader fr = new FileReader("test2.txt");
+        FileWriter fw = new FileWriter("test2.txt", true);) {
+            int character;
+            while((character = fr.read())!=-1) {
+                System.out.print((char) character);
+            }
+            System.out.println();
+            System.out.println("Done!");
+        }
+    }
+}
---
+++ UdemyJavaPro/src/work/chapter_8/tryWithResources/TryWithResourcesExample.java
@@ -0,0 +1,23 @@
+package work.chapter_8.tryWithResources;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TryWithResourcesExample {
+    public static void main(String[] args) throws IOException {
+        String rubai = "Много лет размышлял я над жизнью земной.\n" +
+                        "Непонятного нет для меня под луной.\n" +
+                        "Мне известно, что мне ничего не известно!\n" +
+                        "Вот последняя правда, открытая мной.";
+        String s = "hello";
+        try (FileWriter fw = new FileWriter("test2.txt", true)){
+            for (int i = 0; i < rubai.length(); i++) {
+                fw.write(rubai.charAt(i));
+            }
+            fw.write(s);
+            System.out.println("Done!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
