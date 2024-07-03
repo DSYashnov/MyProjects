6285760686e784ca5bf2f5a4f3ebade22e2e30cf
Stanislav Yashnov <s.yashnov@gmail.com>
2024-07-04 02:02:13 +0300

    Udemy3

---
+++ UdemyJavaPro/src/chapter_8/FileStream/fileInputStream/CopyExample.java
@@ -0,0 +1,26 @@
+package chapter_8.FileStream.fileInputStream;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyExample {
+        public static void main(String[] args) {
+            try (FileInputStream inputStream = new FileInputStream(
+                    "C:\\Users\\syash\\Desktop\\Логотип_Java.png");
+                 FileOutputStream fileOutputStream = new FileOutputStream("java_pic.png");
+            ) {
+                int i;
+                while((i = inputStream.read()) !=-1){
+                    fileOutputStream.write(i);
+                }
+                System.out.println("Done!");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
